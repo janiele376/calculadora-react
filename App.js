a2a2e8d5f2c86939e2003047b4f7aee6644164cc
Janiele <janiele.nascimento.2426@gmail.com>
2025-07-01 11:14:50 -0300

    Minha primeira calculadora

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculadora from './componentes/Calculadora';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculadora />
+    </div>
+  );
+}
+
+export default App;
